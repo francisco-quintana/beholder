af42ead0595ac674e1ffb76c385af8b3d8ae746e
Francisco Quintana <54821161+francisco-quintana@users.noreply.github.com>
2020-09-01 11:26:02 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="h-screen bg-gray-600 relative m-0">
         <div className="m-auto flex flex-col text-2xl text-center items-center justify-center h-screen">
         <div>
-        <p className="font-sans">Aqui se hara gloria</p>
+        <p className="font-sans">Bloc de Notas</p>
         </div>
         </div>
       </div>
